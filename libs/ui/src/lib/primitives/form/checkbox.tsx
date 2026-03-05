451495bb47bb8322a4cd7dc108f46fcc8c04c082
Stepan Ogorodnikov <30865856+ogorodnikov-stepan@users.noreply.github.com>
2026-03-05 18:47:26 +0300

    Add indeterminate state for checkbox

--- libs/ui/src/lib/primitives/form/checkbox.tsx
+++ libs/ui/src/lib/primitives/form/checkbox.tsx
@@ -1,12 +1,12 @@
 import type { TWVProps } from "@koloda/ui";
-import { Check } from "lucide-react";
+import { Check, Minus } from "lucide-react";
 import type { PropsWithChildren } from "react";
 import { Checkbox as ReactAriaCheckbox } from "react-aria-components";
 import type { CheckboxProps as ReactAriaCheckboxProps } from "react-aria-components";
 import { tv } from "tailwind-variants";
 import { label } from "./label";
 
-const checkbox = tv({ base: "group flex flex-row items-center gap-2 min-h-10 rounded-md focus-ring" });
+const checkbox = tv({ base: "group flex flex-row items-center gap-2 min-size-10 rounded-md focus-ring" });
 
 export type CheckboxProps = ReactAriaCheckboxProps & TWVProps<typeof checkbox> & PropsWithChildren;
 
@@ -16,16 +16,23 @@ export function Checkbox({ variants, ...props }: CheckboxProps) {
 
 const checkboxIndicator = [
   "flex items-center justify-center size-5 min-w-5 rounded border-2 border-checkbox bg-checkbox shadow-checkbox",
-  "group-selected:bg-checkbox-selected group-selected:border-checkbox-selected animate-colors",
+  "group-selected:bg-checkbox-selected group-selected:border-checkbox-selected",
+  "group-indeterminate:bg-checkbox-selected group-indeterminate:border-checkbox-selected",
+  "animate-colors",
 ].join(" ");
 
 const checkboxIndicatorCheck = [
   "size-3 min-w-3 stroke-4 fg-checkbox-check opacity-0 group-selected:opacity-100 animate-opacity",
 ].join(" ");
 
+const checkboxIndicatorMinus = [
+  "size-3 min-w-3 stroke-4 fg-checkbox-check opacity-0 group-indeterminate:opacity-100 animate-opacity absolute",
+].join(" ");
+
 function CheckboxIndicator() {
   return (
     <div className={checkboxIndicator}>
+      <Minus className={checkboxIndicatorMinus} />
       <Check className={checkboxIndicatorCheck} />
     </div>
   );
