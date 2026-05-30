0c1a9e5c9ee9d17e9dd09dfa6167960f9bd6f597
Stepan Ogorodnikov <30865856+ogorodnikov-stepan@users.noreply.github.com>
2026-05-30 12:27:02 +0300

    Update table sort icon

--- libs/ui/src/lib/primitives/table/table-sort-icon.tsx
+++ libs/ui/src/lib/primitives/table/table-sort-icon.tsx
@@ -5,8 +5,8 @@ const tableSortIconPath = tv({
   base: "animate-colors",
   variants: {
     isSelected: {
-      true: "stroke-fg-table-head",
-      false: "stroke-fg-table-head-inactive",
+      true: "stroke-fg-level-2",
+      false: "stroke-fg-disabled",
     },
   },
 });
@@ -16,14 +16,29 @@ type TableSortIconProps = { sorting?: SortDirection | false };
 export function TableSortIcon({ sorting }: TableSortIconProps) {
   return (
     <svg
-      className="size-4 mt-0.25 stroke-2 fill-none"
+      className="size-4 min-w-4 rotate-90"
       xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="none"
+      color="currentColor"
+      stroke="currentColor"
+      stroke-width="2.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      aria-hidden="true"
     >
-      <path className={tableSortIconPath({ isSelected: sorting === "asc" })} d="m7 15 5 5 5-5" />
-      <path className={tableSortIconPath({ isSelected: sorting === "desc" })} d="m7 9 5-5 5 5" />
+      <path
+        className={tableSortIconPath({ isSelected: sorting === "desc" })}
+        d="M8.99995 7L6.62858 8.92711C4.87619 10.3512 4 11.0633 4 12C4 12.9367 4.8762 13.6488 6.62859 15.0729L9 17"
+      >
+      </path>
+      <path
+        className={tableSortIconPath({ isSelected: sorting === "asc" })}
+        d="M15 7L17.3714 8.92711C19.1238 10.3512 20 11.0633 20 12C20 12.9367 19.1238 13.6488 17.3714 15.0729L15 17"
+      >
+      </path>
     </svg>
   );
 }
