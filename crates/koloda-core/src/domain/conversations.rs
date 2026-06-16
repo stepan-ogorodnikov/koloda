5a42b7acb73e51f6acb1387c8f6fe42b8aebda83
Stepan Ogorodnikov <30865856+ogorodnikov-stepan@users.noreply.github.com>
2026-06-16 21:47:03 +0300

    Add conversation timestamp deserialization for rust backend

--- crates/koloda-core/src/domain/conversations.rs
+++ crates/koloda-core/src/domain/conversations.rs
@@ -1,15 +1,26 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-use crate::app::utility::{default_now, serialize_optional_timestamp, serialize_timestamp};
+use crate::app::utility::{
+    default_now, deserialize_optional_timestamp, deserialize_timestamp, serialize_optional_timestamp,
+    serialize_timestamp,
+};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Conversation {
     pub id: String,
     pub state: Value,
-    #[serde(default = "default_now", serialize_with = "serialize_timestamp")]
+    #[serde(
+        default = "default_now",
+        serialize_with = "serialize_timestamp",
+        deserialize_with = "deserialize_timestamp"
+    )]
     pub created_at: i64,
-    #[serde(default, serialize_with = "serialize_optional_timestamp")]
+    #[serde(
+        default,
+        serialize_with = "serialize_optional_timestamp",
+        deserialize_with = "deserialize_optional_timestamp"
+    )]
     pub updated_at: Option<i64>,
 }
