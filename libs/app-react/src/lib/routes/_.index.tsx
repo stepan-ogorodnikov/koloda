1b86cedf15adadce358b256b638f96fb5d9c46bc
Stepan Ogorodnikov <30865856+ogorodnikov-stepan@users.noreply.github.com>
2026-09-16 12:35:01 +0300

    Drop unreachable index route redirect check

--- libs/app-react/src/lib/routes/_.index.tsx
+++ libs/app-react/src/lib/routes/_.index.tsx
@@ -1,15 +1,11 @@
 import { useTitle } from "@koloda/core-react";
 import { useQueryClient } from "@tanstack/react-query";
-import { createFileRoute, redirect, useNavigate } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { useEffect } from "react";
 import { appMenu } from "../components/app";
 
 export const Route = createFileRoute("/_/")({
   component: IndexRoute,
-  beforeLoad: ({ location, context: { queryClient } }) => {
-    const data = queryClient.getQueryData(["app"]);
-    if (data === "ok" && location.pathname === "/_/") throw redirect({ to: appMenu[0].to, replace: true });
-  },
 });
 
 function IndexRoute() {
