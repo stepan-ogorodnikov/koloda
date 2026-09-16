938585acf9cd48357daa35bcc9bf81e3f7205e04
Stepan Ogorodnikov <30865856+ogorodnikov-stepan@users.noreply.github.com>
2026-09-16 17:04:04 +0300

    Fix typecheck

--- libs/app-react/src/lib/routes/_.decks.$deckId.tsx
+++ libs/app-react/src/lib/routes/_.decks.$deckId.tsx
@@ -41,7 +41,7 @@ function DeckRoute() {
   return (
     <Tabs defaultSelectedKey="cards">
       <Layout.Header>
-        <Layout.H1 variants={{ grow: false }}>{query.data?.title}</Layout.H1>
+        <Layout.H1 variants={{ isGrow: false }}>{query.data?.title}</Layout.H1>
         {query.data && (
           <Tabs.List aria-label={_(msg`deck.tabs.label`)}>
             {DECK_TABS.map(({ id, t }) => (
